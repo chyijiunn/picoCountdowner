aeecccc52e0a298e1c105d5a79ffeb6abeef1420
chyijiunn <chyijiunn@gmail.com>
2024-04-19 12:24:02 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from machine import Pin ,PWM ,Timer
+from utime import sleep
+servo = PWM(Pin(0))
+buzzer = PWM(Pin(14))
+LED = Pin(1,Pin.OUT)#控制LED
+servo.freq(50)
+LED.value(1)
+
+minset = 30
+duty = 1900
+dutyini = 1900
+motorend = 8450
+tic = (motorend - duty) /minset /60 #每秒動多少
+
+def beep():
+    for j in range(2):
+        for i in range(4):
+            buzzer.freq(4000)
+            buzzer.duty_u16(20000)
+            sleep(0.15)
+            buzzer.duty_u16(0)
+            sleep(0.01)
+        sleep(0.8)
+
+def motor(tim):
+    global duty
+    servo.duty_u16(int(duty))
+    duty = duty + tic
+    if duty >= motorend:
+        duty = dutyini
+        servo.duty_u16(dutyini)
+        beep()
+        
+    restime = (motorend-duty)/tic
+    restmin = int(restime // 60)
+    restsec = int(restime % 60)
+    print(restmin,':',restsec,duty)
+
+tim = Timer(-1)
+tim.init(period=1000, mode=Timer.PERIODIC, callback=motor)
